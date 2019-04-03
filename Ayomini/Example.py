52491e42284ae13bbd963c4fae41f6448541498b
Ayomini <ayominiogunmuyiwa@yahoo.com>
2019-04-03 21:21:34 +0300

    My example of turtle solution

---
+++ Ayomini/Example.py
@@ -0,0 +1,31 @@
+class Turtle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_left(self, steps):
+        final_position = self.x - steps
+        if final_position < 0:
+            final_position = 0
+        self.x = final_position
+
+    def move_right(self, steps):
+        final_position = self.x + steps
+        if final_position > 10:
+            final_position = 10
+        self.x = final_position
+
+    def move_up(self, steps):
+        final_position = self.y + steps
+        if final_position > 10:
+            final_position = 10
+        self.y = final_position
+
+    def move_down(self, steps):
+        final_position = self.y - steps
+        if final_position < 0:
+            final_position = 0
+        self.y = final_position
+
+    def get_position(self):
+        return self.x, self.yt
