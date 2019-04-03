36c511d6ccfa3c2dc6fbc9465a8e67af9e7c33ac
yusuf2407 <yusuf.monaf@holidu.com>
2019-04-03 19:36:59 +0200

    Yusuf TODO program

---
+++ Yusuf/ToDo_List.py
@@ -0,0 +1,16 @@
+class TODO:
+    def __init__(self, item_number, description):
+        self.item_number = item_number
+        self.description = description
+
+    def add_item(self, description):
+
+
+    def mark_item_as_done(self, item_number):
+
+
+    def delete_item(self, item_number):
+
+
+    def list_items(self, show_done=False):
+
