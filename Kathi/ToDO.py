bfb69b2824c89a575b557e908ea8b93b96248079
Kathi <Katharina.Kauntz@gmx.de>
2019-04-03 19:33:55 +0200

    My To Do

---
+++ Kathi/ToDO.py
@@ -0,0 +1,24 @@
+#To Do List
+
+#add_item(description)
+#mark_item_as_done(item_number)
+#delete_item(item_number)
+#list_items(show_done=False)
+
+
+class ToDo:
+    def __init__(self,item_number,description):
+        self.item_number = item_number
+        self.description = description
+
+    def add_item(self,description):
+
+
+    def mark_item_as_done(self,item_number):
+
+
+    def delete_item(self, item_number):
+
+
+    def list_items(self,show_done):
+
