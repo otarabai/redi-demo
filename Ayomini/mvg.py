981664f3cd9bce1b6dd746a8ef1a78aeccca546a
Ayomini <ayominiogunmuyiwa@yahoo.com>
2019-04-17 22:10:20 +0300

    lets go home now

---
+++ Ayomini/mvg.py
@@ -0,0 +1,6 @@
+import mvg_api
+
+x=mvg_api.get_id_for_station("Hohenzollern Platz")
+print(x)
+
+
--- Ayomini/updated_mvg.py
+++ Ayomini/updated_mvg.py
@@ -1,12 +1,22 @@
 import navigate
 from pprint import pprint
+import googlemaps
+import mvg_api
 
 #x = input("enter name of station")
 #depatures = navigate.get_id_of_station(station_id)
 
 #starting_station_id = navigate.get_id_of_station(starting_destination_name)
 #departures = navigate.get_departure(station_name)
-starting_destination_name= input('Enter name of station(starting point)')
+
+gmaps = googlemaps.Client(key='AIzaSyDJ-s_kYjvu_g9VCQ8FuW0H2bQFTDUoc9U')
+my_location = gmaps.geolocate()['location']
+nearby_stations = mvg_api.get_nearby_stations(my_location['lat'], my_location['lng'])
+print("The closest station is: ", nearby_stations[0]['name'])
+
+### TODO use the method  closest_station( lat, lon):
+
+starting_destination_name= nearest_stations[0]['name']
 final_destination_name = input("enter name of station (destination name)")
 #final_station_id = navigate.get_id_of_station(final_destination)
 
@@ -20,13 +30,3 @@ tickets = first_route['tickets']
 
 print(navigate.convert_from_epoch(first_route['departure']))
 print(navigate.convert_from_epoch(first_route['arrival']))
-print("rings change : " + str(first_route['ringFrom']) + ' to ' + str(first_route['ringTo']))
-print("zug from platform" + departure_platform + " in the direction of " + richtung)
-
-for ticket in tickets:
-    print(ticket['displayText'] +  '=>' + str(ticket['fare']) + '' + ticket['currency'] )
-print()
-
-#for departure in departures:
-#   print(departure['departureId'])
-#print(departures)
